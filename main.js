1950ee957cc03113f9e60b5e0ca82d1fe1a5df10
DIFU-N <chukwudifunsofor@gmail.com>
2022-11-01 13:23:55 +0100

    responsive buttons

---
+++ main.js
@@ -0,0 +1,19 @@
+let mobile = document.querySelector(".mobile"),
+    laptop = document.querySelector(".laptop"),
+    tablet = document.querySelector(".tablet");
+
+
+mobile.addEventListener("click", function (e) {
+    e.preventDefault();
+    console.log('sa');
+});
+
+laptop.addEventListener("click", function (e) {
+    e.preventDefault();
+    console.log('sa');
+});
+
+tablet.addEventListener("click", function (e) {
+    e.preventDefault();
+    console.log('sa');
+});
